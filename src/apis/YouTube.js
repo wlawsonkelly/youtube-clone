ec5472974b47ab141b993eb543cebec86fe1267c
Lawson Kelly <wlawsonkelly@gmail.com>
2020-07-28 17:00:54 -0400

    got rid of key

--- src/apis/YouTube.js
+++ src/apis/YouTube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDQcDy9oW3pu9CY-rS2wGcNr4rQqwJIrNM';
+const KEY = "Enter your key"
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
